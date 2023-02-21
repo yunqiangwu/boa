42bddccba9859d46ad7e4674d4803bbf1cf285ab
yunqiang.wu <yunqiang.wu@hiand-china.com>
2023-02-22 03:00:13 +0800

    fix: 升级 conda 版本

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 export GIT_MIRROR=https://ghproxy.com/
 export BOA_TUNA=1
 export BOA_PYTHON_VERSION=3.9
-export BOA_CONDA_VERSION='Miniconda3-py39_4.9.2'
+export BOA_CONDA_VERSION='Miniconda3-py39_4.12.0'
 export BOA_CONDA_MIRROR=https://mirrors.bfsu.edu.cn/anaconda/miniconda
 
 yarn run build
